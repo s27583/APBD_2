a7ffcb4d41555f566c23f7253d7e3305d65f428c
Mikołaj Wróblewski <s27583@pjwstk.edu.pl>
2024-03-08 14:45:11 +0100

    tested

--- ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e9294ebecdbc663cda3498abb531bae0edb77e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085de6decb798b1086f4423bf0122eef1550569e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
