7b7fa142edc6294df4f37b16fb49e7d30d45518a
s27583 <s27583@pjwstk.edu.pl>
2024-03-13 20:08:49 +0100

    feature average

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,7 +5,6 @@ Console.WriteLine("Hello, World!");
 int[] tab = { 1, 2, 3, 4, 5 };
 
 Console.WriteLine(GetAvg([1,2,3,4,5]));
-Console.WriteLine(GetMax([1,2,3,4,5]));
 
 static double GetAvg(int[] tab) {
     int sum = 0;
@@ -17,14 +16,4 @@ static double GetAvg(int[] tab) {
     }
 
     return (double)sum / tab.Length;
-}
-
-static int GetMax(int[] tab) {
-    int max = 0;
-    foreach (var num in tab) {
-        if (num > max) {
-            max = num;
-        }
-    }
-    return max;
 }
